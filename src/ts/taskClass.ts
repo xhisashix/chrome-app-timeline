ec594e516940692c47bb2b76659691cd86c47546
Hisashi Iwamoto <hisasi622@gmail.com>
2023-11-26 20:42:39 +0900

    Refactor taskClass to implement taskInterface and add addTask method

--- src/ts/taskClass.ts
+++ src/ts/taskClass.ts
@@ -1,10 +1,56 @@
 import storageClass from "./storageClass";
 import moment from "moment";
 
-class taskClass {
+interface taskInterface {
+  id: number;
+  name: string;
+  start_time: string;
+  end_time: string;
+  elapsed_time: string;
+}
+
+class taskClass implements taskInterface {
+  public id: number;
+  public name: string;
+  public start_time: string;
+  public end_time: string;
+  public elapsed_time: string;
+
   protected storage: storageClass;
   constructor() {
     this.storage = new storageClass();
+    this.id = 0;
+    this.name = "";
+    this.start_time = "";
+    this.end_time = "";
+    this.elapsed_time = "";
+  }
+
+  public addTask(taskName: string): void {
+    let taskList: taskInterface[] = [];
+
+    this.storage.getStorage("taskList", (result: string) => {
+      taskList = this.getTaskList(result);
+
+      const newTask: taskInterface = {
+        id: taskList.length,
+        name: taskName,
+        start_time: this.formatTime(this.roundTime(this.getNow())),
+        end_time: "",
+        elapsed_time: "",
+      };
+
+      taskList.push(newTask);
+      this.storage.saveToStorage("taskList", JSON.stringify(taskList));
+    });
+  }
+
+  private getTaskList(result: string): taskInterface[] {
+    if (result) {
+      return JSON.parse(result);
+    } else {
+      return [];
+    }
   }
 
   /**
@@ -20,7 +66,7 @@ class taskClass {
   /**
    * 時刻を15分単位で切り捨てる
    * @param {moment.Moment} time
-   * @return {void}
+   * @return {moment.Moment}
    */
   public roundTime(time: moment.Moment): moment.Moment {
     const rounded = time.minutes(Math.floor(time.minutes() / 15) * 15);
